5df81de5b3e3bc6762aa1f09fab8a4997e93db48
solpinetree <sadlthf@icloud.com>
2022-05-29 15:21:46 +0900

    #merge

--- src/main/java/com/aj22/foodlab/service/RestaurantService.java
+++ src/main/java/com/aj22/foodlab/service/RestaurantService.java
@@ -25,12 +25,29 @@ public class RestaurantService {
 		return restaurants;
 	}
 	
+	public List<RestaurantDTO> selectList_category(Pagination pagination, String category) throws SQLException{
+		List<RestaurantDTO> restaurants = null;
+		RestaurantDAO dao = new RestaurantDAOImpl();
+		restaurants = dao.selectList_category(pagination.getFirstReviewId(), pagination.getNumOfRecordsPerPage(), category);
+		dao.close();
+		return restaurants;
+	}
+	
+	
 	public List<String> getCategories() throws SQLException{
 		List<String> categories = null;
 		RestaurantDAO dao = new RestaurantDAOImpl();
 		categories = dao.selectCategories();
 		dao.close();
 		return categories;
+	}
+	
+	public List<RestaurantDTO> selectByCategory(String category) throws SQLException{
+		List<RestaurantDTO> restaurants = null;
+		RestaurantDAO dao = new RestaurantDAOImpl();
+		restaurants = dao.select(category);
+		dao.close();
+		return restaurants;
 	}
 	
 	public Integer getRestaurantIdFromName(String name) throws SQLException {
@@ -48,6 +65,15 @@ public class RestaurantService {
 		dao.close();
 		return restaurant;
 	}
+	
+	public List<RestaurantDTO> selectByName(String name) throws SQLException{
+		List<RestaurantDTO> restaurants = null;
+		RestaurantDAO dao = new RestaurantDAOImpl();
+		restaurants = dao.select_name(name);
+		dao.close();
+		return restaurants;
+	}
+	
 
 	public int getNumOfRecord() throws SQLException {
 		int cnt = 0;
@@ -66,6 +92,16 @@ public class RestaurantService {
 		pagination.pageInfo(currentPage, numOfRecords, NumOfRecordsPerPage);
 		
 		return pagination;
+	}
+	
+	public int getNumOfRecord_category(String category) throws SQLException {
+		int cnt = 0;
+		
+		RestaurantDAO dao = new RestaurantDAOImpl();
+		cnt = dao.countRecords_category(category);
+		dao.close();
+		
+		return cnt;
 	}
 	
 	
