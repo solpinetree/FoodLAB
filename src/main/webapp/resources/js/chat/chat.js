dd03f61a8378a1821474fcd2dcad14193a6ae71e
Shimeungi <103654354+Shimeungi@users.noreply.github.com>
2022-06-06 17:48:00 +0900

    Chat syncronized
    정말 끝!

--- src/main/webapp/resources/js/chat/chat.js
+++ src/main/webapp/resources/js/chat/chat.js
@@ -20,24 +20,45 @@ websocket.onclose = function(message){}
 websocket.onerror = function(message){}
 //	onmessage는 외부에서 들어온 데이터를 받아서 처리를 하는 이벤트이다.
 
-websocket.onmessage = function(message)
-{
+websocket.onmessage = function(message) {
+	
 	console.log("js.websocket.onmessage ON");
-	addChat(message.data);
-	gotoBottom(chatWindow); // 입력했을 때 스크롤하단으로
+	
+	var chatmsg = message.data;
+	
+	if(chatmsg.substring(0,6) == 'server'){
+		console.log("reciever");
+		addChatReceiver(message.data);
+		gotoBottom(chatWindow); // 입력했을 때 스크롤하단으로
+	} else{
+		console.log("send");
+		addChat(message.data);
+		gotoBottom(chatWindow); // 입력했을 때 스크롤하단으로
+	}
+	//서버로부터 메세지를 받았을 때 작동되는 함수
+
+	
+	
 }
 
 function sendmessage(){
-	var msg = chatName.value + ":" + chatContent.value;
-	websocket.send(msg);
-	msgDB_insert(); // DB 에 msg insert by ajax
+	if(chatContent.value == ""){
+		//메세지를 입력하지않고 버튼을 누른 경우
+	} else {
+		var msg = chatName.value + ":" + chatContent.value;
+		websocket.send(msg);
+		msgDB_insert(); // DB 에 msg insert by ajax
+	}
+	
 	
 	chatContent.value = ""; //	메시지를 보내고, 메시지 입력 부분을 지워준다. 
 }
 
 // websocket onmessage 에서 호출하는 함수
 // article 단위로 메시지를 생성한다.
+
 function addChat(message){
+	
 	var msg_origin = message;
 	var msg_origin = msg_origin.split(':');
 	var nowTime = currentTime();
@@ -56,7 +77,7 @@ function addChat(message){
 			+			'<p class = "msg" id = "chat">' + msg_origin[1] + '</p>'
 			+		'</div>' 
 			+		'<span class = "timestamp">'
-			+			'<span class = "username">' + chatName.value + '</span>' + "&bull;" 
+			+			'<span class = "username">' + msg_origin[0] + '</span>' + "&bull;" 
 			+			'<span class = "posttime">' + nowTime + '</span>'
 			+		'</span>'
 			+ 	'</div>' 
@@ -65,6 +86,42 @@ function addChat(message){
 	
 	el.innerHTML = itemStr;
 	el.setAttribute("class", "msg-container msg-self");
+	
+	console.log(el);
+	rmsg.appendChild(el);
+}
+
+
+function addChatReceiver(message){
+	
+	var msg_origin = message;
+	var msg_origin = msg_origin.split(':');
+	var nowTime = currentTime();
+	// : 을 기준으로 split 
+	// msg_origin[0] : server
+	// msg_origin[1] : chatName
+	// msg_origin[2] : chatContent
+	 
+
+	var rmsg = document.getElementById('chat-window');
+	console.log("addChatReceiver(id, msg)->" + message);
+	var el = document.createElement('article'); // <div> element 생성
+	
+	itemStr = '<div class="msg-box">'
+			+	'<div class = "flr">'
+			+		'<div class = "messages">' 
+			+			'<p class = "msg" id = "chat">' + msg_origin[2] + '</p>'
+			+		'</div>' 
+			+		'<span class = "timestamp">'
+			+			'<span class = "username">' + msg_origin[1] + '</span>' + "&bull;" 
+			+			'<span class = "posttime">' + nowTime + '</span>'
+			+		'</span>'
+			+ 	'</div>' 
+			+	'<img class = "user-img" id = "user-0" src = "//gravatar.com/avatar/56234674574535734573000000000001?d=retro" />'
+			+ '</div>';
+	
+	el.innerHTML = itemStr;
+	el.setAttribute("class", "msg-container msg-other");
 	
 	console.log(el);
 	rmsg.appendChild(el);
